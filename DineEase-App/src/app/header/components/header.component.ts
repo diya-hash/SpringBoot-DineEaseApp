9b7e82b936df3ab774175a1d39e3300637aec153
Diya Salam <sdiya.salam.github@gmail.com>
2025-02-26 18:05:05 -0500

    add

--- DineEase-App/src/app/header/components/header.component.ts
+++ DineEase-App/src/app/header/components/header.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  username = "AndrewBarnes"
+  username = 'JohnDoe'; 
 }
